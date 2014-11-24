af58b73f9dcdfcd00d406527efb93b7b574200f2
Kalimaha <guido.barbaglia@gmail.com>
2014-11-24 15:00:22 +0100

    PostgreSQL connector.

--- geobricks_dbms/core/dbms_core.py
+++ geobricks_dbms/core/dbms_core.py
@@ -1,4 +1,5 @@
 from geobricks_dbms.core.dbms_mongodb import DBMSMongoDB
+from geobricks_dbms.core.dbms_postgresql import DBMSPostgreSQL
 
 
 class DBMS():
@@ -6,16 +7,21 @@ class DBMS():
     # User's parameters.
     db_type = None
     db_name = None
+    username = None
+    password = None
     collection_name = None
 
     # Computed parameters
     mongodb = None
+    postgresql = None
 
-    def __init__(self, db_type, db_name, collection_name):
+    def __init__(self, db_type, db_name, username=None, password=None, collection_name=None):
 
         # Store user's parameters.
         self.db_type = db_type
         self.db_name = db_name
+        self.username = username
+        self.password = password
         self.collection_name = collection_name
 
         # Connect to the DB
@@ -24,24 +30,21 @@ class DBMS():
     def connect(self):
         if 'mongodb' in self.db_type:
             self.mongodb = DBMSMongoDB(self.db_name, self.collection_name)
+        elif 'postgresql' in self.db_type:
+            self.postgresql = DBMSPostgreSQL(self.db_name, self.username, self.password)
 
-    def find(self):
+    def select_all(self, table_name=None):
         if 'mongodb' in self.db_type:
             return self.mongodb.find()
+        elif 'postgresql' in self.db_type:
+            return self.postgresql.select_all(table_name)
 
     def find_by_id(self, item_id):
         if 'mongodb' in self.db_type:
             return self.mongodb.find_by_id(item_id)
 
-    def insert(self, item):
+    def insert(self, item, table_name=None):
         if 'mongodb' in self.db_type:
             return self.mongodb.insert(item)
-
-
-mydb = DBMS('mongodb', 'test', 'posts')
-mydb.insert({"pippo": "pluto"})
-for post in mydb.find():
-    print post
-print
-for post in mydb.find_by_id('54730665f8cd671e687311e8'):
-    print post
+        if 'postgresql' in self.db_type:
+            return self.postgresql.insert(table_name, item)
---
+++ geobricks_dbms/core/dbms_postgresql.py
@@ -0,0 +1,75 @@
+import psycopg2
+from types import DictType
+
+
+class DBMSPostgreSQL:
+
+    # User's parameters
+    db_name = None
+    username = None
+    password = None
+    host = None
+    port = None
+
+    # Computed variables.
+    connection = None
+
+    def __init__(self, db_name, username, password, host='localhost', port=5432):
+
+        # Store user parameters.
+        self.db_name = db_name
+        self.username = username
+        self.password = password
+        self.host = host
+        self.port = port
+
+        # Connect to the DB
+        self.connect()
+
+    def connect(self):
+        try:
+            self.connection = psycopg2.connect(self.get_connection_string())
+            self.connection.autocommit = True
+        except:
+            raise Exception('Unable to connect to the DB.')
+
+    def query(self, sql):
+        cur = self.connection.cursor()
+        cur.execute(sql)
+        return cur.fetchall()
+
+    def select_all(self, table_name):
+        cur = self.connection.cursor()
+        cur.execute('SELECT * FROM ' + table_name)
+        return cur.fetchall()
+
+    def insert(self, table_name, item):
+        sql = ''
+        if type(item) is DictType:
+            sql += "INSERT INTO " + table_name + " ("
+            for key in item:
+                sql += key + ','
+            sql = sql[0:len(sql) - 1]
+            sql += ") VALUES ("
+            for key in item:
+                sql += "'" + item[key] + "',"
+            sql = sql[0:len(sql) - 1]
+            sql += ")"
+        else:
+            sql = item
+        cur = self.connection.cursor()
+        return cur.execute(sql)
+
+    def get_connection_string(self):
+        return "host=%s port='%s' dbname=%s user=%s password=%s" % \
+               (self.host, self.port, self.db_name, self.username, self.password)
+
+    def close_connection(self):
+        if self.connection is not None:
+            self.connection.close()
+
+    def __del__(self):
+        self.close_connection()
+
+    def __exit__(self):
+        self.close_connection()
