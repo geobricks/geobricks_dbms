c9edde011557d919e725fb1c74f52b7e0f203015
simonefenix <simone.murzilli@fao.org>
2015-07-22 17:35:51 +0200

    update

---
+++ geobricks_dbms/config/config.py
@@ -0,0 +1,30 @@
+config = {
+    "settings": {
+        # To be used by Flask: DEVELOPMENT ONLY
+        "debug": True,
+
+        # Flask host: DEVELOPMENT ONLY
+        "host": "localhost",
+
+        # Flask port: DEVELOPMENT ONLY
+        "port": 5925,
+
+        "db": {
+            # Spatial Database
+            "spatial": {
+                # default_db will search in the dbs["database"] as default option
+                "dbname": "spatialdbtest",
+                "host": "localhost",
+                "port": "5432",
+                "username": "postgres",
+                "password": "",
+                "schema": "spatial",
+                "tables": {
+                    "country": {
+                        "table": "ne_110m_admin_0_countries"
+                    }
+                }
+            }
+        }
+    }
+}
--- test/__init__.py
+++
@@ -1,4 +0,0 @@
-__package__ = 'test'
-__author__ = 'Barbaglia, Guido - Murzilli, Simone'
-__email__ = 'guido.barbaglia@gmail.com; simone.murzilli@gmail.com;'
-__license__ = 'GPL2'
